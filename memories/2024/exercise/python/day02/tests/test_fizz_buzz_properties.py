44e164689dbcf5fb16b0dd465c8f34db96644e2e
Fabrice Bibonne <fabrice.bibonne@courriel.eco>
2025-12-03 09:18:24 +0100

    Work for day  2
    PR #2

--- memories/2024/exercise/python/day02/tests/test_fizz_buzz_properties.py
+++ memories/2024/exercise/python/day02/tests/test_fizz_buzz_properties.py
@@ -6,22 +6,24 @@ from games.fizz_buzz import FizzBuzz, MIN, MAX
 
 
 class FizzBuzzProperties(unittest.TestCase):
-    fizz_buzz_strings = ["Fizz", "Buzz", "FizzBuzz"]
+    fizz_buzz_strings = ["Fizz", "Buzz", "Whizz", "Bang", "FizzBuzz", "WhizzBang", "BuzzWhizz", "BuzzBang", "FizzBang", "FizzWhizz"]
 
     def valid_strings_for(self, x: int) -> list:
         return self.fizz_buzz_strings + [str(x)]
 
     @given(st.integers(min_value=MIN, max_value=MAX))
     def test_parse_returns_valid_string_for_numbers_between_1_and_100(self, x):
+        fizzBuzz = FizzBuzz({11: "Bang", 3: "Fizz", 7: "Whizz", 5: "Buzz"})
         self.assertIn(
-            FizzBuzz.convert(x),
+            fizzBuzz.convert(x),
             self.valid_strings_for(x)
         )
 
     @given(st.integers().filter(lambda i: i < MIN or i > MAX))
     def test_parse_fails_for_numbers_out_of_range(self, x):
+        fizzBuzz = FizzBuzz({11: "Bang", 3: "Fizz", 7: "Whizz", 5: "Buzz"})
         self.assertIsNone(
-            FizzBuzz.convert(x)
+            fizzBuzz.convert(x)
         )
 
 
